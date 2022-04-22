bc517c4dc76667a399e1a57b226e063c794bd8bd
Rajashekarredde <32640055+Rajashekarredde@users.noreply.github.com>
2022-04-21 19:37:24 -0700

    gift msg issues

--- frontend/src/components/Purchases.js
+++ frontend/src/components/Purchases.js
@@ -8,6 +8,8 @@ import AirportShuttleIcon from "@mui/icons-material/AirportShuttle";
 import Navbar from "./Navbar";
 import Posts from './Posts';
 import Pagination from './Pagination';
+import DropdownButton from 'react-bootstrap/DropdownButton';
+import Dropdown from 'react-bootstrap/Dropdown'
 
 function Purchases() {
   const user = useSelector(selectUser);
@@ -15,7 +17,7 @@ function Purchases() {
   const [posts, setPosts] = useState([]);
   const [loading, setLoading] = useState(false);
   const [currentPage, setCurrentPage] = useState(1);
-  const [postsPerPage] = useState(1);
+  const [postsPerPage, setPostPerPage] = useState(5);
 
   useEffect(() => {
     Axios.get("/getPurchasedItems/"  + user.id ).then((response) => {
@@ -38,19 +40,40 @@ function Purchases() {
   // Change page
   const paginate = pageNumber => setCurrentPage(pageNumber);
 
+  const [value,setValue]=useState('');
+  const handleSelect=(e)=>{
+    console.log(e);
+    setPostPerPage(e)
+    setValue(e)
+  }
+
   return(
+   
     <>
       <Navbar />
-            
+      <DropdownButton
+      style={{marginLeft: "1000px", marginBottom : "-80px"}}
+      title="Items Per Page"
+      id="dropdown-menu-align-right"
+      onSelect={handleSelect}
+        >
+              <Dropdown.Item eventKey="1">1</Dropdown.Item>
+              <Dropdown.Item eventKey="2">2</Dropdown.Item>
+              <Dropdown.Item eventKey="5">5</Dropdown.Item>
+              <Dropdown.Item eventKey="10">10</Dropdown.Item>
+      </DropdownButton>
+
        <div className='container mt-5'>
       <h1 className='mb-3' style={{textDecoration:"underline overline", color:"rgb(255, 81, 0)"}}>Your Purchases</h1>
       <Posts posts={currentPosts} loading={loading} />
       <br></br>
+
       <Pagination
         postsPerPage={postsPerPage}
         totalPosts={posts.length}
         paginate={paginate}
       />
+
     </div>
     </>
   );
